ae0c5fe806a80b5b2fd49a3a2d957326fe666ace
Shrey <svaity@stevens.edu>
2020-04-27 19:57:49 -0400

    FInal COmmit with bugfixes

--- react-client/src/components/Shared/Error.js
+++ react-client/src/components/Shared/Error.js
@@ -4,19 +4,19 @@ import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
 
 const Error = ({ classes, error }) => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(true)
   return (
-    <snackbar
+    <Snackbar
       open={open}
       className={classes.snackbar}
       message={error.message}
       action={
-        <Button onClick={() => setOpen(false)} color="secoondary" size="small">
+        <Button onClick={() => setOpen(false)} color="secondary" size="small">
           Close
         </Button>
       }
     />
-  );
+  )
 };
 
 const styles = theme => ({
